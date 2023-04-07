f4c52cf069b002cf5b1b33b1afd4ec89b0426698
yeremygarcia <jeremyisgarcia@gmail.com>
2023-04-06 21:54:46 -0400

    1

---
+++ tip.js
@@ -0,0 +1,22 @@
+// Variable holding the total amount of the bill (before the tip)
+let totalBillAmount = 200;
+
+// Function to calculate the tip amount
+function calculateTip(amount) {
+  const tipAmount = amount * 0.2; // 20% tip
+  return tipAmount;
+}
+
+// Function to get the total bill amount w/ tip
+function getBillTotal(amount) {
+  let tipAmount = calculateTip(totalBillAmount);
+  let totalAmount = amount + tipAmount;
+  return totalAmount;
+}
+
+// Console log the results of the functions
+console.log("The tip amount is $" + calculateTip(totalBillAmount));
+
+console.log(
+  `The total bill amount (including tip) is $${getBillTotal(totalBillAmount)}`
+);
